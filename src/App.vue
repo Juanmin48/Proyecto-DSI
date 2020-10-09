0f13ae6c0b391b4f7c61ba7ea3a6d570174f414c
Jonathan Caballero <jonathancaballero@uninorte.edu.co>
2020-10-08 21:08:06 -0500

    Ejemplos de usos

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/services">Services</router-link>
+    </div>
+    <router-view/>
   </div>
-  <router-view/>
 </template>
 
 <style>
